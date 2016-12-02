384c7e65313e17f34890b3ce8895f5b3cfdebc19
liberaliscomputing <liberalis.computing@gmail.com>
2016-12-01 19:27:17 -0500

    complete the series data structure

---
+++ coursera/introduction-to-data-science-in-python/week-02-basic-data-processing-with-pandas/code/series_data_structure.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+The Series Data Structure
+December 1, 2016
+Coded by Meen Chul Kim
+'''
+
+import pandas as pd
+
+# Explore series data structure
+animals = ['Tiger', 'Bear', 'Moose']
+print pd.Series(animals)
+
+numbers = [1, 2, 3]
+print pd.Series(numbers, dtype='f')
+
+sports = {
+	'Archery': 'Bhutan',
+	'Golf': 'Scotland',
+	'Sumo': 'Japan',
+	'Taekwondo': 'South Korea'}
+s = pd.Series(sports, index=['Golf', 'Sumo', 'Hockey'])
+print s
