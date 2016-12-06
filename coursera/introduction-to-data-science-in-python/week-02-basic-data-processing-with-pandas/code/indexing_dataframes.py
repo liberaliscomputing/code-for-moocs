f8c56206f5c215b8a101fad7c33d4a2aa7c4fd66
liberaliscomputing <liberalis.computing@gmail.com>
2016-12-05 20:07:03 -0500

    add exercise of multiple indexes

--- coursera/introduction-to-data-science-in-python/week-02-basic-data-processing-with-pandas/code/indexing_dataframes.py
+++ coursera/introduction-to-data-science-in-python/week-02-basic-data-processing-with-pandas/code/indexing_dataframes.py
@@ -70,3 +70,32 @@ print df.loc[key_1]
 # Query a DataFrame with multiple keys
 key_2 = 'Michigan', 'Wayne County'
 print df.loc[[key_1, key_2]]
+
+# Exercise
+purchase_1 = pd.Series({
+	'Name': 'Chris',
+	'Item Purchased': 'Dog Food',
+	'Cost': 22.50})
+purchase_2 = pd.Series({
+	'Name': 'Kevyn',
+	'Item Purchased': 'Kitty Litter',
+	 'Cost': 2.50})
+purchase_3 = pd.Series({
+	'Name': 'Vinod',
+	'Item Purchased': 'Bird Seed',
+	'Cost': 5.00})
+
+df = pd.DataFrame([purchase_1, purchase_2, purchase_3],
+	index=['Store 1', 'Store 1', 'Store 2'])
+
+df['Location'] = df.index
+df = df.set_index(['Location', 'Name'])
+df = df.append(pd.Series(data={
+	'Cost': 3.00, 
+	'Item Purchased': 'Kitty Food'}, 
+	name=('Store 2', 'Kevyn')))
+print df
+
+# Another solution
+# df = df.set_index([df.index, 'Name'])
+# df.index.names = ['Location', 'Name']
