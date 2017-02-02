a5d9a5d1e0d6283ee4e4d57dfcc1b22d46299423
liberaliscomputing <liberalis.computing@gmail.com>
2017-02-02 17:09:19 -0500

    modify linked_list.py

--- udemy/algorithms-and-data-structures-in-python/linked_list.py
+++ udemy/algorithms-and-data-structures-in-python/linked_list.py
@@ -1,112 +1,71 @@
 # -*- coding: utf-8 -*-
 
 class Node(object):
-
-	def __init__(self, data):
-		self.data = data
-		self.next = None
-
+    
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+        
 class LinkedList(object):
+    
+  def __init__(self):
+    self.head = None
+    self.size = 0
+  
+  def insert(self, data, index=0):
+    if index > self.size:
+      return 'IndexError: list out of range'
+    
+    node = Node(data)
+    curr = self.head
+    prev = None
 
-	def __init__(self):
-		self.head = None
-		self.size = 0
+    for i in range(index):
+      prev = curr
+      curr = curr.next
 
-	def insert_start(self, data):
-		node = Node(data)
+    node.next = curr
+    
+    if not prev:
+      self.head = node
+    else:
+      prev.next = node
 
-		if not self.head:
-			self.head = node
-		else:
-			node.next = self.head
-			self.head = node
+    self.size += 1
+      
+  def delete(self, data):
+    if self.size == 0:
+      return 'LinkedList is empty.'
 
-		self.size += 1
+    curr = self.head
+    prev = None
+    
+    while data != curr.data:
+      prev = curr
+      curr = curr.next
 
-	def insert_end(self, data):
-		node = Node(data)
-		curr = self.head
+      if not curr:
+        return 'No such item.'
+        
+    if not prev:
+      self.head = curr.next
+    else:
+      prev.next = curr.next
+        
+    self.size -= 1
+          
+  def traverse(self):
+    if self.size == 0:
+      return 'LinkedList is empty.'
+    
+    curr = self.head
 
-		if not curr:
-			self.head = node
-		else:
-			while curr.next:
-				curr = curr.next
-			curr.next = node
-
-		self.size += 1
-
-	def insert_at(self, data, index):
-		if index == 0:
-			self.insert_start(data)
-		elif index > self.size:
-			return 'Error: the index is out of range.'
-		else:
-			node = Node(data)
-			prev = None
-			curr = self.head
-			i = 0
-
-			while i < index:
-				prev = curr
-				curr = curr.next
-				i += 1
-
-			node.next = curr
-			prev.next = node
-
-			self.size += 1
-
-	def delete_start(self):
-		if self.head != None:
-			if self.head.next:
-				self.head = self.head.next
-			else:
-				self.head = None
-
-			self.size -= 1
-
-	def delete_end(self):
-		if self.head != None:
-			prev = None
-			curr = self.head
-
-			while curr.next:
-				prev = curr
-				curr = curr.next
-
-			if not prev:
-				self.head = None
-			else:
-				prev.next = None
-
-			self.size -= 1
-
-	def delete(self, data):
-		if self.head != None:
-			prev = None
-			curr = self.head
-
-			while curr.data != data:
-				prev = curr
-				curr = curr.next
-
-			if not prev:
-				self.head = None
-			else:
-				prev.next = curr.next
-
-			self.size -= 1
-
-	def traverse(self):
-		curr = self.head
-
-		while curr:
-			print '{}'.format(curr.data)
-			curr = curr.next
-
-	def get_size(self):
-		return self.size
+    while curr:
+      print '{}'.format(curr.data)
+      curr = curr.next
+          
+  def get_size(self):
+    return self.size
 
 
 if __name__ == '__main__':
